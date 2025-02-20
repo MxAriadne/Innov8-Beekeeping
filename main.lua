12f6f192667e1c6327a6c9596c3880ea0b703574
Leviathynn <j.stephanie.zhang@gmail.com>
2025-02-20 10:36:08 -0600

    Background and base colliders

--- main.lua
+++ main.lua
@@ -1,20 +1,90 @@
-function love.load(arg)
-    x = 100
-    y = 50
+--alt+L to run love2d games
+
+print('test')
+
+--libraries import from libraries folder
+local sti = require 'libraries/sti'
+local wf = require 'libraries/windfield'
+
+function love.load()
+
+    --base size for the game right now
+    --will put everything into variables later so it can be more easily resized
+    money = 0
+    hive = love.graphics.newImage('sprites/hive.png')
+    map = sti('maps/TilesForBeekeepingGameTopBoundaries.lua')
+    love.window.setMode(960, 640)
+
+    world = wf.newWorld()
+
+    --player creation and collider configuation
+    player = {}
+    player.collider = world:newBSGRectangleCollider(480, 340, 20, 20, 14)
+    player.collider:setFixedRotation(true)
+    player.x = 480
+    player.y = 320
+    player.speed = 300
+
+    --auto-getting and making the colliders from the objects created in Tiled
+    walls = {}
+    if map.layers["Walls"] then
+        for i, obj in pairs(map.layers["Walls"].objects) do
+            local wall = world:newRectangleCollider(obj.x*2, obj.y*2, obj.width*2, obj.height*2)
+            wall:setType('static')
+            table.insert(walls, wall)
+        end
+    end
+
+    --collider for the beehive itself
+    local wall = world:newRectangleCollider(80, 225, 120, 135 )
+    wall:setType('static')
+
 end
 
 function love.update(dt)
+
+    --player movement with colliders
+    world:update(dt)
+    player.x = player.collider:getX()
+    player.y = player.collider:getY()
+
+    local vx = 0
+    local vy = 0
     if love.keyboard.isDown("right") then
-        x = x + 100 * dt
-    elseif love.keyboard.isDown("left") then
-        x = x - 100 * dt
-    elseif love.keyboard.isDown("up") then
-        y = y - 100 * dt
-    elseif love.keyboard.isDown("down") then
-        y = y + 100 * dt
+        vx = player.speed
     end
+
+    if love.keyboard.isDown("left") then
+        vx = player.speed * -1
+    end
+
+    if love.keyboard.isDown("up") then
+        vy = player.speed * -1
+    end
+
+    if love.keyboard.isDown("down") then
+        vy = player.speed
+    end
+
+    player.collider:setLinearVelocity(vx, vy)
+
+end
+
+--still making this
+function love.keypressed(key)
+   if (key == "e") then
+      money = money + 5
+      print(money)
+   end
 end
 
 function love.draw()
-    love.graphics.rectangle("line", x, y, 200, 150)
+    map:draw(0, 0, 2, 2)
+    love.graphics.draw(hive, 200, 225, 0, -1, 1)
+
+    world:draw()--makes the colliders visible, for debugging - comment out later
+    love.graphics.circle("line", player.x, player.y, 20)
+    --hiveTagColor = {.3, 0, 0, 1}
+    --love.graphics.print({hiveTagColor, "Beehive: Level 1"}, 70, 195, 0, 1.5, 1.5)
+
 end
